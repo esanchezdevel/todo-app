3893b9411b0068e3feb6fccf3dfc3fc4adb3a84b
enrique.sanchez <enrique.sanchez@hpe.com>
2025-02-04 16:06:12 +0100

    feat: add app icon

--- src/main/java/com/todo/todoapp/JavaFxApplication.java
+++ src/main/java/com/todo/todoapp/JavaFxApplication.java
@@ -9,6 +9,7 @@ import com.todo.todoapp.application.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 @SpringBootApplication
@@ -26,10 +27,12 @@ public class JavaFxApplication extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		System.out.println("Starting application...");
-		
+
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/main-view.fxml"));
 		loader.setControllerFactory(SpringContext::getBean); // Use Spring for controllers
 		Scene scene = new Scene(loader.load(), Constants.APP_WIDTH, Constants.APP_HEIGHT);
+
+		stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/app_icon.png")));
 
 		stage.setTitle(Constants.APP_TITLE + " - " + Constants.APP_VERSION);
 		stage.setResizable(false);
