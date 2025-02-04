69115d251276e7dffb82a557b6b8002d76a8e09d
enrique.sanchez <enrique.sanchez@hpe.com>
2025-02-04 13:09:47 +0100

    feat: show the current category and status of one task in task details view

--- src/main/java/com/todo/todoapp/controller/TaskDetailsViewController.java
+++ src/main/java/com/todo/todoapp/controller/TaskDetailsViewController.java
@@ -1,13 +1,18 @@
 package com.todo.todoapp.controller;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.todo.todoapp.domain.model.Task;
+import com.todo.todoapp.domain.model.TasksStatus;
+import com.todo.todoapp.domain.service.CategoriesService;
 import com.todo.todoapp.domain.service.LoadViewService;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -25,10 +30,13 @@ public class TaskDetailsViewController {
 	@FXML
 	private TextField nameField;
 	@FXML
-	private ComboBox categoryComboBox, statusComboBox;
+	private ComboBox<String> categoryComboBox, statusComboBox;
 
 	@Autowired
 	private LoadViewService loadViewService;
+
+	@Autowired
+	private CategoriesService categoriesService;
 
 	@FXML
 	private void initialize () {
@@ -39,8 +47,33 @@ public class TaskDetailsViewController {
 		nameField.setText(String.valueOf(task.getTitle()));
 		nameField.setMinWidth(550);
 		nameField.setMaxWidth(550);
+
 		categoryLabel.setText("Category:");
+		categoryLabel.setMinWidth(200);
+		categoryLabel.setMaxWidth(200);
+		
 		statusLabel.setText("Status:");
+		statusLabel.setMinWidth(200);
+		statusLabel.setMaxWidth(200);
+
+		categoryComboBox.setStyle("-fx-font-size: 12px; " +
+			"-fx-padding: 5px 5px 5px 5px; " + // top right bottom left
+			"-fx-background-color: #ADD8E6;");
+		categoryComboBox.setMaxWidth(500);
+		categoryComboBox.setMinWidth(500);
+		categoryComboBox.setItems(FXCollections.observableArrayList(categoriesService.getAllNames()));
+		categoryComboBox.setValue(task.getCategory());
+
+		statusComboBox.setStyle("-fx-font-size: 12px; " +
+			"-fx-padding: 5px 5px 5px 5px; " + // top right bottom left
+			"-fx-background-color: #ADD8E6;");
+		statusComboBox.setMaxWidth(120);
+		statusComboBox.setMinWidth(120);
+		statusComboBox.setItems(FXCollections.observableArrayList(List.of(TasksStatus.TODO.value(), 
+																			TasksStatus.IN_PROGRESS.value(), 
+																			TasksStatus.DONE.value(), 
+																			TasksStatus.CANCELLED.value())));
+		statusComboBox.setValue(task.getStatus().value());
 	}
 
 	@FXML
