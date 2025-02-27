050485188607e7d884de29c2627640faa493ddd0
enrique.sanchez <enrique.sanchez@hpe.com>
2025-02-27 16:31:37 +0100

    feat: resize window height in base of screen dimensions

--- src/main/java/com/todo/todoapp/JavaFxApplication.java
+++ src/main/java/com/todo/todoapp/JavaFxApplication.java
@@ -7,9 +7,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import com.todo.todoapp.application.Constants;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 @SpringBootApplication
@@ -30,7 +34,7 @@ public class JavaFxApplication extends Application {
 
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/main-view.fxml"));
 		loader.setControllerFactory(SpringContext::getBean); // Use Spring for controllers
-		Scene scene = new Scene(loader.load(), Constants.APP_WIDTH, Constants.APP_HEIGHT);
+		Scene scene = new Scene(loader.load());
 
 		stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/app_icon.png")));
 
@@ -38,7 +42,47 @@ public class JavaFxApplication extends Application {
 		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.show();
+
+		        // Adjust the window size based on the current screen
+        adjustWindowSize(stage);
+
+        // Add listeners to detect when the window is moved
+        ChangeListener<Number> positionListener = new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                adjustWindowSize(stage);
+            }
+        };
+
+		stage.xProperty().addListener(positionListener);
+		stage.yProperty().addListener(positionListener);
 	}
+
+    // Method to adjust the window size based on the screen
+    private void adjustWindowSize(Stage stage) {
+        // Get the screen where the window is currently displayed
+        Screen screen = Screen.getScreensForRectangle(
+                stage.getX(), 
+                stage.getY(), 
+                stage.getWidth(), 
+                stage.getHeight()
+        ).get(0);
+
+        // Get the screen's visible bounds
+        Rectangle2D bounds = screen.getVisualBounds();
+
+		double windowWidth = 825.0;
+		double windowHeight = 0.0;
+		if (bounds.getHeight() >= 1000.0) {
+			windowHeight = 850.0;
+		} else {
+			windowHeight = 800.0;
+		}
+        
+        // Apply the new size to the Stage
+        stage.setWidth(windowWidth);
+        stage.setHeight(windowHeight);
+    }
 
 	@Override
 	public void stop() throws Exception {
--- src/main/java/com/todo/todoapp/application/Constants.java
+++ src/main/java/com/todo/todoapp/application/Constants.java
@@ -1,12 +1,10 @@
 package com.todo.todoapp.application;
 
+import javafx.stage.Screen;
+
 public class Constants {
 	
 	public static final String APP_TITLE = "DoItNow!";
 	
 	public static final String APP_VERSION = "v3.0-SNAPSHOT";
-
-	public static final int APP_WIDTH = 800;
-	
-	public static final int APP_HEIGHT = 800;
 }
